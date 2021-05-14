235d39cbc1a3bc9784f275d7435426c43cadc440
Muhammed Didin <mdidin80@gmail.com>
2021-05-14 18:40:50 +0300

    Update Product

--- src/main/java/com/example/webservice/entity/Product.java
+++ src/main/java/com/example/webservice/entity/Product.java
@@ -1,4 +1,9 @@
 package com.example.webservice.entity;
 
 public class Product {
+    private String model;
+    private String brand;
+    private int price;
+    private String label;
+    private String screensize;
 }
