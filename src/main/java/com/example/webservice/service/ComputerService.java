f79ecc0ffc4729b2fa7d1212a4df822693e28833
edakaradavut <53890734+edakaradavut@users.noreply.github.com>
2021-05-29 18:39:04 +0300

    Services implemented

--- src/main/java/com/example/webservice/service/ComputerService.java
+++ src/main/java/com/example/webservice/service/ComputerService.java
@@ -1,7 +1,24 @@
 package com.example.webservice.service;
 
+import com.example.webservice.entity.Computer;
+import com.example.webservice.entity.Product;
+import com.example.webservice.repository.ComputerRepository;
+import com.example.webservice.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ComputerService {
+    @Autowired
+    ComputerRepository computerRepository;
+
+    public Computer saveComputer(Computer c)
+    {
+        return computerRepository.save(c);
+    }
+
+    public Computer getComputer(int id)
+    {
+        return computerRepository.findById(id).orElse(null);
+    }
 }
