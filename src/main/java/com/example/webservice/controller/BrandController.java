bfc2efd6be1e089dbc38fff94c4bf04cf5322f16
Muhammed Didin <mdidin80@gmail.com>
2021-06-01 14:40:15 +0300

    add brand, brand service and brand repository. Get and Post services are implemented

--- src/main/java/com/example/webservice/controller/BrandController.java
+++ src/main/java/com/example/webservice/controller/BrandController.java
@@ -1,9 +1,13 @@
 package com.example.webservice.controller;
 
 import com.example.webservice.entity.Brand;
+import com.example.webservice.entity.Computer;
+import com.example.webservice.entity.Product;
 import com.example.webservice.service.BrandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class BrandController {
@@ -14,4 +18,15 @@ public class BrandController {
     public  Brand saveBrand(@RequestBody Brand brand){
         return brandService.saveBrand(brand);
     }
+
+    @GetMapping("/getbrand/{id}")
+    public Brand getComputer(@PathVariable int id){
+        return brandService.getBrand(id);
+    }
+
+    @GetMapping("/getbrandalldetails")
+    public List<Brand> getComputerDetails(){
+        return brandService.getBrandDetails();
+    }
+
 }
--- src/main/java/com/example/webservice/repository/BrandRepository.java
+++ src/main/java/com/example/webservice/repository/BrandRepository.java
@@ -8,5 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 public interface BrandRepository extends JpaRepository<Brand, Integer>{
-
+    @Query("SELECT b FROM Brand b")
+    public List<Brand> getBrandDetails();
 }
