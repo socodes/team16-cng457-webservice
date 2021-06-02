e334893aa7367c2ae0de898754e30b99ba463fb5
edakaradavut <53890734+edakaradavut@users.noreply.github.com>
2021-06-02 20:18:41 +0300

    Comment And Rate(Rate Query)
    -selection for comment rate
    -solved repository controller and service issue

--- src/main/java/com/example/webservice/controller/CommentAndRateController.java
+++ src/main/java/com/example/webservice/controller/CommentAndRateController.java
@@ -28,4 +28,28 @@ public class CommentAndRateController {
         return CommentAndRateService.getCommentAndRateDetails();
     }
 
+    @GetMapping("/getcommentandrate1")
+    public List<CommentAndRate> getCommentAndRate1(){
+        return CommentAndRateService.getCommentAndRate1();
+    }
+
+    @GetMapping("/getcommentandrate2")
+    public List<CommentAndRate> getCommentAndRate2(){
+        return CommentAndRateService.getCommentAndRate2();
+    }
+
+    @GetMapping("/getcommentandrate3")
+    public List<CommentAndRate> getCommentAndRate3(){
+        return CommentAndRateService.getCommentAndRate3();
+    }
+
+    @GetMapping("/getcommentandrate4")
+    public List<CommentAndRate> getCommentAndRate4(){
+        return CommentAndRateService.getCommentAndRate4();
+    }
+
+    @GetMapping("/getcommentandrate5")
+    public List<CommentAndRate> getCommentAndRate5(){
+        return CommentAndRateService.getCommentAndRate5();
+    }
 }
--- src/main/java/com/example/webservice/service/CommentAndRateService.java
+++ src/main/java/com/example/webservice/service/CommentAndRateService.java
@@ -1,8 +1,6 @@
 package com.example.webservice.service;
 
-
 import com.example.webservice.entity.CommentAndRate;
-import com.example.webservice.repository.BrandRepository;
 import com.example.webservice.repository.CommentAndRateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -28,4 +26,22 @@ public class CommentAndRateService {
         return CommentAndRateRepository.getCommentAndRateDetails();
     }
 
+    public List<CommentAndRate> getCommentAndRate1(){
+        return CommentAndRateRepository.getCommentAndRate1();
+    }
+
+    public List<CommentAndRate> getCommentAndRate2(){
+        return CommentAndRateRepository.getCommentAndRate2();
+    }
+
+    public List<CommentAndRate> getCommentAndRate3(){
+        return CommentAndRateRepository.getCommentAndRate3();
+    }
+
+    public List<CommentAndRate> getCommentAndRate4(){
+        return CommentAndRateRepository.getCommentAndRate4();
+    }
+    public List<CommentAndRate> getCommentAndRate5(){
+        return CommentAndRateRepository.getCommentAndRate5();
+    }
 }
